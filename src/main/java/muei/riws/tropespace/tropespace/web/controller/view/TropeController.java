ddfd88e6b7712a893f8d7cf178ddddcd55c6514e
DiegoMartinezCela <diego.martinez.cela@udc.es>
2019-11-22 18:28:27 +0100

    Agregado esqueleto para estadísticas de medios.

--- src/main/java/muei/riws/tropespace/tropespace/web/controller/view/TropeController.java
+++ src/main/java/muei/riws/tropespace/tropespace/web/controller/view/TropeController.java
@@ -1,14 +1,11 @@
 package muei.riws.tropespace.tropespace.web.controller.view;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
